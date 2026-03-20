528de8ca86091d9459945999965ab2e8758dc72a
LudoO <ludovic.valente@gmail.com>
2026-03-21 00:29:23 +0100

    Add LinkedHashSet import to C4ReportTreeMerger for enhanced data handling

--- archimo/src/main/java/fr/geoking/archimo/extract/C4ReportTreeMerger.java
+++ archimo/src/main/java/fr/geoking/archimo/extract/C4ReportTreeMerger.java
@@ -10,6 +10,7 @@ import fr.geoking.archimo.extract.model.report.DiagramIndexSlot;
 
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
